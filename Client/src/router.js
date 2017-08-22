4a6614809fd851756ee7266513368970cb957f49
Yubin Kim <joeekimm@gmail.com>
2017-08-21 17:15:46 -0700

    removed EBS folder and added a few routes to the routing component

--- Client/src/router.js
+++ Client/src/router.js
@@ -8,6 +8,8 @@ import {
 
 import App from './Components/App';
 import Projects from './Components/Projects';
+import Resume from './Components/Resume';
+import AboutMe from './Components/AboutMe';
 
 const Router = () => {
   return (
@@ -15,6 +17,8 @@ const Router = () => {
       <Switch>
         <Route exact path="/" component={App}/>
         <Route exact path='/projects' component={Projects}/>
+        <Route exact path='/resume' component={Resume}/>
+        <Route exact path='/aboutme' component={AboutMe}/>
       </Switch>
     </BrowserRouter>
   )
