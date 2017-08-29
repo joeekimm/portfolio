f447b1b0c42f6af8c4f82e0dfd7cd8552fb43d9b
Yubin Kim <joeekimm@gmail.com>
2017-08-28 19:02:55 -0700

    testing something in ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps: [{
-    name: 'portfolio',
-    script: './Server/server.js'
-  }],
+  apps: [
+    {
+      name: "portfolio",
+      script: "./Server/server.js"
+    }
+  ],
   deploy: {
     production: {
-      user: 'ec2-user',
-      host: 'ec2-34-210-81-52.us-west-2.compute.amazonaws.com',
-      key: '~/.ssh/portfolio.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:joeekimm/portfolio.git',
-      path: '/home/ec2-user/portfolio',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      user: "ec2-user",
+      host: "ec2-34-210-81-52.us-west-2.compute.amazonaws.com",
+      key: "~/.ssh/portfolio.pem",
+      ref: "origin/master",
+      repo: "git@github.com:joeekimm/portfolio.git",
+      path: "/home/ec2-user/portfolio",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
-}
+};
