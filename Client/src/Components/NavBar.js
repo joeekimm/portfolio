ffa0117fcc1ac26540c3ec04d61d212f407d699e
Yubin Kim <joeekimm@gmail.com>
2017-09-25 19:44:42 -0700

    changed the theme of the site to black and white and refactored some of the code

--- Client/src/Components/NavBar.js
+++ Client/src/Components/NavBar.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
 
 import Projects from './Projects';
 import Resume from './Resume';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      scroll: false
+    };
+
+    this.handleNavScroll = this.handleNavScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleNavScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleNavScroll);
+  }
+
+  handleNavScroll() {
+    if (!document.body.scrollTop) {
+      this.setState({ scroll: false });
+    } else {
+      this.setState({ scroll: true });
+    }
+  } 
+  
   render() {
     return (
-      <div className='navbar-container'>
-        <div className='navbar aboutme'>
-          <Link to="/aboutme" className='navbar-text'>About Me</Link>
-        </div>
-        <div className='navbar projects'>
-          <Link to="/projects" className='navbar-text'>Projects</Link>
-        </div>
-        <div className='navbar resume'>
-          <Link to="/resume" className='navbar-text'>Resume</Link>
-        </div>
+      <div className={this.state.scroll ? 'nav-scroll': 'navbar-container'}>
+        <button onClick={() => this.props.handlePageScroll('aboutme')}>
+          <text className='navbar-text'>About Me</text>
+        </button>
+        <button onClick={() => this.props.handlePageScroll('projects')}>
+          <text className='navbar-text'>Projects</text>
+        </button>
+        <button onClick={() => this.props.handlePageScroll('resume')}>
+          <text className='navbar-text'>Resume</text>
+        </button>
       </div>
     );
   }
