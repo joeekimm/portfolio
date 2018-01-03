1c5540150505470a7fc31f7bfdb5794ed50bacf1
Yubin Kim <joeekimm@gmail.com>
2018-01-03 03:01:43 -0800

    added functionality for the hamburger-style side menu, such as onclick, opennav/closenav

--- Client/src/Components/App.js
+++ Client/src/Components/App.js
@@ -9,6 +9,7 @@ import NavBar from "./NavBar";
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Resume from './Resume';
+import MenuButton from './MenuButton';
 // import Metronome from './Metronome';
 
 class App extends Component {
@@ -16,10 +17,14 @@ class App extends Component {
     super(props);
 
     this.state = {
+      menu: "menu-container",
+      visible: false
     };
 
     this.toTop = this.toTop.bind(this);
     this.handlePageScroll = this.handlePageScroll.bind(this);
+    this.hamburgerClick = this.hamburgerClick.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
   }
 
   componentDidMount() {
@@ -33,11 +38,26 @@ class App extends Component {
     TweenLite.to(window, 0.75, { scrollTo: `#${id}` });
   }
 
+  hamburgerClick(event) {
+    this.toggleMenu();
+
+    console.log("hamburger clicked");
+    let css =
+      this.state.menu === "menu-container" ? "change" : "menu-container";
+    this.setState({ menu: css });
+    event.stopPropagation();
+  }
+
+  toggleMenu() {
+    this.setState({ visible: !this.state.visible });
+  }
+
   render() {
     return (
       <div className='main'>
-        <Particles params={particlesJSON} style={{position: 'fixed', margin: '0', marginLeft: '-50%', padding: '0', zIndex: '-1'}} height={'100vh'} width={'100vw'}/>
-        <NavBar handlePageScroll={this.handlePageScroll} toTop={this.toTop}/>
+        <NavBar hamburgerClick={this.hamburgerClick} menuVisibility={this.state.visible} handlePageScroll={this.handlePageScroll} toTop={this.toTop}/>
+        {/* <Particles params={particlesJSON} style={{position: 'fixed', margin: '0', marginLeft: '-50%', padding: '0', zIndex: '-1'}} height={'100vh'} width={'100vw'}/> */}
+        <MenuButton hamburgerClick={this.hamburgerClick} menu={this.state.menu}/>
         <div className='main home' id='home'>
           {/* <Metronome /> */}
           <Typist className='typist' avgTypingDelay={100} startDelay={700} cursor={{show: false}} onTypingDone={() => console.log('typing done')}>
--- Client/src/Components/NavBar.js
+++ Client/src/Components/NavBar.js
@@ -11,71 +11,76 @@ class NavBar extends Component {
       scroll: false
     };
 
-    this.handleNavScroll = this.handleNavScroll.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleNavScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleNavScroll);
-  }
-
-  handleNavScroll() {
-    let x, y;
-
-    if ( typeof window.pageYOffset === 'number' ) {
-      x = window.pageXOffset;
-      y = window.pageYOffset;
-      // console.log('in offset, ', y);
-    } else {
-      x = document.body.scrollLeft;
-      y = document.body.scrollTop;
-    }
-
-    if (y <= 10) {
-      this.setState({ scroll: false });
-    } else {
-      this.setState({ scroll: true });
-    }
-  }
+  componentDidMount() {}
 
   handleClick(section) {
     this.props.handlePageScroll(section);
   }
 
   render() {
+    let visibility = "hide";
+
+    if (this.props.menuVisibility) {
+      visibility = "show";
+    }
+
     return (
-      <div className={this.state.scroll ? "nav-scroll" : "navbar-container"}>
-        <button
-          className="nav-button"
-          id="grid-aboutme"
-          onClick={() => this.handleClick("aboutme")}
-        >
-          About Me
-        </button>
-        <button
-          className="nav-button"
-          id="grid-projects"
-          onClick={() => this.handleClick("projects")}
-        >
-          Projects
-        </button>
-        <button
-          className="nav-button"
-          id="grid-resume"
-          onClick={() => this.handleClick("resume")}
-        >
-          Resume
-        </button>
-        <button
-          className="nav-button"
-          id="grid-top"
-          onClick={() => this.props.toTop("home")}
-        >
-          Top
-        </button>
+      <div
+        id="navbar-container"
+        onMouseDown={this.props.handleMouseDown}
+        className={visibility}
+      >
+        <h2>
+          <a
+            className="nav-button"
+            id="grid-aboutme"
+            onClick={() => {
+              this.handleClick("aboutme");
+              this.props.hamburgerClick();
+            }}
+          >
+            About Me
+          </a>
+        </h2>
+        <h2>
+          <a
+            className="nav-button"
+            id="grid-projects"
+            onClick={() => {
+              this.handleClick("projects");
+              this.props.hamburgerClick();
+            }}
+          >
+            Projects
+          </a>
+        </h2>
+        <h2>
+          <a
+            className="nav-button"
+            id="grid-resume"
+            onClick={() => {
+              this.handleClick("resume");
+              this.props.hamburgerClick();
+            }}
+          >
+            Resume
+          </a>
+        </h2>
+        <h2>
+          <a
+            className="nav-button"
+            id="grid-top"
+            onClick={() => {
+              this.props.toTop("home");
+              this.props.hamburgerClick();
+            }}
+          >
+            Top
+          </a>
+        </h2>
       </div>
     );
   }
